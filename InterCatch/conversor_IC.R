257ffd8ed52b1e13ec03baa7a885ca3e03fcd04b
Arocha <alberto.jmas.rocha@gmail.com>
2024-04-23 00:16:01 +0100

    pequenas correccoes

--- InterCatch/conversor_IC.R
+++ InterCatch/conversor_IC.R
@@ -5,15 +5,16 @@
 
 # 1 - Comparar desembarques oficiais com estimativas de desembarque vendas-dia
 
+options(scipen=999)
 library(dplyr)
 library(ggplot2)
 source('C://repos/path.R'); path('remote')
 land = read.csv(paste0(dados, 'desembarques//desemb_pescart.csv'),
                 sep = ",", dec = ".") %>% 
-  mutate(CODFAO = gsub(" ", "", CODFAO),
-         EARTE = case_when(EARTE == 3 ~ 'OTB',
-                           EARTE == 5 ~ 'PS',
-                           EARTE == 13 ~ 'MIS_MIS'))
+  mutate(CODFAO = gsub(" ", "", CODFAO))
+         # EARTE = case_when(EARTE == 3 ~ 'OTB',
+         #                   EARTE == 5 ~ 'PS',
+         #                   EARTE == 13 ~ 'MIS_MIS'))
 # land_ceph = read.csv(paste0(dados, 'desembarques//desemb_pescart_cephs.csv'),
 #                 sep = ",", dec = ".") %>% 
 #   mutate(CODFAO = gsub(" ", "", CODFAO),
@@ -29,7 +30,7 @@ fao = c('COM','IIL','SQC','OCT','OMZ','CTC',
   'SQM','YHT','OQD','SQZ','CTL','TDQ','EDT','SQA','OJJ','SQL','CTR','SQU')
 
 
-
+# Testes com os vendas-dia
 
 ## 2014
 ano = 2011
@@ -70,9 +71,86 @@ ggplot(get(paste0('teste_',ano)),
 
 
 
-ano = 2022
+# Esforço
+# Truque desta porra: =SUMPRODUCT($F$3:$F$85;U3:U85)
+
+## Importado dos ficheiros da anaclaudia
+eff_2017_otb = c(377191.02,	493060.08,	528667.18,	531837.43,	625891.31,	632292.6,	
+                 598235.83,	644739.61,	458304.55,	522066.86,	648917.02,	507136.98)
 
 
+eff_2018_otb = c(381782.79, 553844.4, 361335.86, 591484.42, 678998.07, 615657.79,	
+                 629923.5,	636089.19,	585944.97,	562302.07,	616083.22,	490958.91)
+
+
+eff_2019_otb = c(380019.72,	556086.54,	546200.9,	534997.05,	653688.15,	611779.68,
+                 618916.01,	619984.51,	584020.52,	626603.27,	535101.15,	413464.09)
+
+
+eff_2020_otb = c(415755.33,	592467.89,	499867.28,	406003.28,	556921.97,	594070.02,
+                 625816.43,	625791.31,	609973.44,	589952.74,	534219.12,	449839.87)
+
+#estes dados foram compilados das colunas mais à direita (corrigidos)
+eff_2021_otb = c(330691.65,	529048.52,	600447.96,	589681.13,	668378.81,	648442.64,
+                 653517.72,	683334.84,	678622.36,	649475.02,	657045.63,	542219.97)
+
+#estes dados foram tirados do separador kw_dia_prt
+eff_2022_otb = c(409828.64,	590481.81,	502122.73,	467467.71,	671088.46,	600995.18,
+                 99825.38,	639218.6,	604762.2,	562030.2,	571539.3,	469840.41)
+
+eff_2023_otb = c(365089.13,	542691.96,	575226.87,	439673.38,	673302,	598206.35,
+                 657755.42,	657517.59,	627709.53,	540380.84,	606937.37,	497296.51)
+
+sum(eff_2023_otb)  ## confirmacao se esta ok
+
+
+eff_n =
+rbind(eff_2017_otb,
+      eff_2018_otb,
+      eff_2019_otb,
+      eff_2020_otb,
+      eff_2021_otb,
+      eff_2022_otb,
+      eff_2023_otb) %>% data.frame %>% 
+  cbind(c(2017:2023)) %>%
+  cbind(rep("OTB",7)) %>% 
+  cbind(rep("27.9.a.c.n",7))
+
+names(eff_n) = c(1:12,'year',"metier",'zona')
+
+eff_s =
+  rbind(eff_2017_otb,
+        eff_2018_otb,
+        eff_2019_otb,
+        eff_2020_otb,
+        eff_2021_otb,
+        eff_2022_otb,
+        eff_2023_otb) %>% data.frame %>% 
+  cbind(c(2017:2023)) %>%
+  cbind(rep("OTB",7)) %>% 
+  cbind(rep("27.9.a.c.s",7))
+
+names(eff_s) = c(1:12,'year',"metier",'zona')
+
+eff_a =
+  rbind(eff_2017_otb,
+        eff_2018_otb,
+        eff_2019_otb,
+        eff_2020_otb,
+        eff_2021_otb,
+        eff_2022_otb,
+        eff_2023_otb) %>% data.frame %>% 
+  cbind(c(2017:2023)) %>%
+  cbind(rep("OTB",7)) %>% 
+  cbind(rep("27.9.a.s.a",7))
+
+names(eff_a) = c(1:12,'year',"metier",'zona')
+
+eff = rbind(eff_n, eff_s, eff_a)
+
+eff = reshape2::melt(eff, id = c('metier', 'zona', 'year'))
+names(eff) = c('metier', 'zona', 'year', 'mes', 'effort')
+eff$mes = as.numeric(as.character(eff$mes))
 
 
 # Importa dados
@@ -128,6 +206,8 @@ fao = c("SQC","OCT","OMZ","CTC",
         "CTL","TDQ","EDT","SQA",
         "OJJ","SQL","CTR","SQU")
 
+ano = 2023
+
 # transforma tabela
 land_export =
 land %>%
@@ -151,18 +231,20 @@ land %>%
   filter(zona != 'O')
 
 
-land_export =
-  get(paste0('vd_', ano)) %>% 
-  mutate(EESPECIE = case_when(EESPECIE %in% fao ~ EESPECIE,
-                              T ~ 'OTH')) %>% 
-  group_by(year_sale, zona, EESPECIE, month_sale, EGRUPART) %>% 
-  summarise(QVENDA = sum(QVENDA))
 
-effort = 
-  get(paste0('vd_', ano)) %>% 
-  group_by(year_sale, zona, month_sale, EGRUPART, PORTO, IDATVEND, IEMBARCA) %>% 
-  summarise(kw_day = unique(Power.Main))
-
+# Quando desembarques se obtiverem a partir das vendas-dia:
+# land_export =
+#   get(paste0('vd_', ano)) %>% 
+#   mutate(EESPECIE = case_when(EESPECIE %in% fao ~ EESPECIE,
+#                               T ~ 'OTH')) %>% 
+#   group_by(year_sale, zona, EESPECIE, month_sale, EGRUPART) %>% 
+#   summarise(QVENDA = sum(QVENDA))
+# 
+# effort = 
+#   get(paste0('vd_', ano)) %>% 
+#   group_by(year_sale, zona, month_sale, EGRUPART, PORTO, IDATVEND, IEMBARCA) %>% 
+#   summarise(kw_day = unique(Power.Main))
+# Fim do troço vendas-dia
 
 
 # land_export = land %>% filter(COD_FAO %in% fao)
@@ -172,12 +254,18 @@ land_export[land_export$CODFAO == 'SQU',]$CODFAO = 'OMZ'
 land_export$zona = as.character(land_export$zona)
 
 # save(land, file="C://Google Drive//Polvices//WGCEPH 2020//desemb_mes_2019.Rdata")
-write.csv(land, file = 'intercatch_2023_raw.csv')
+# write.csv(land, file = 'intercatch_2024_raw.csv')
 
-for(j in unique(land_export$COD_FAO)){
+
+#corrido temporariamente enquanto nao mandaram dataset completo do esforço
+land_export = land_export %>% 
+  filter(EARTE == 'OTB')
+
+#cria ficheiro IC
+for(j in unique(land_export$CODFAO)){
   teste_9 = data.frame()
   teste_10 = data.frame()
-  occ = land_export[land_export$COD_FAO==j,]
+  occ = land_export[land_export$CODFAO==j,]
   for(i in 1:nrow(occ)){
     linha =
       paste("HI,","PT,",ano,",","Month,",
@@ -185,7 +273,10 @@ for(j in unique(land_export$COD_FAO)){
         occ$EARTE[i],",",
         ifelse(occ$zona[i] == '27.10',"SubArea,","AreaUnit,"),
         occ$zona[i],",",
-        "NA,","NA,",effort,"NA",
+        "NA,","NA,",eff[eff$metier==occ$EARTE[i] &
+                          eff$mes == occ$IMES[i] &
+                          eff$zona == occ$zona[i] &
+                          eff$year == ano,]$effort,",kWd",
         "\n",
         "SI,","PT,",ano,",","Month,",
         occ$IMES[i],",",
@@ -193,7 +284,7 @@ for(j in unique(land_export$COD_FAO)){
         ifelse(occ$zona[i] == '27.10',"SubArea,","AreaUnit,"),
         occ$zona[i],",",
         "NA,",
-        occ$COD_FAO[i],",",
+        occ$CODFAO[i],",",
         "NA,","L,","R,","NA,","H,","U,","NA,","t,",
         occ$QESTIMADA[i]/1000,",",
         occ$QESTIMADA[i]/1000,",",
@@ -217,5 +308,5 @@ write.table(teste_10,
 # resumo de desembarques OTB
 land_export %>% 
   filter(ARTE_EU == 'MIS_MIS_0_0_0') %>% 
-  group_by(COD_FAO) %>%
+  group_by(CODFAO) %>%
   summarise(tons = sum(QESTIMADA/1000))
